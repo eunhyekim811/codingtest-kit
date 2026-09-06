3c75068906769fe646457d420808735d4f4eabc8
Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>
2026-09-06 23:44:47 +0900

    solve: H-Index (정호성)

--- week04_sorting/정호성/H-Index_정호성.java
+++ week04_sorting/정호성/H-Index_정호성.java
@@ -1,18 +1,6 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        Arrays.sort(citations);
-        
-        for(int i = 0; i < citations.length; ++i) {
-            int h = citations.length - i;
-            
-            if(citations[i] >= h) {
-                return h;
-            }
-        }
         return answer;
     }
 }
